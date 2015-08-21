5e8b72d331e2aa53de3c4fff3c90951d2245644f
Evan Brown <evanbrown@google.com>
2015-08-21 10:20:09 -0700

    Update build node

--- flow.groovy
+++ flow.groovy
@@ -1,4 +1,4 @@
-node('gcp-packer') {
+node('docker') {
   git url: 'https://github.com/GoogleCloudPlatform/scalable-resilient-web-app.git'
   def app = docker.build 'scalable-resilient-web-app'
   app.withRun {c ->
