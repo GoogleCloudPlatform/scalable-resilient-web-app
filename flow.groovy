948d5058faae1f143237daecb06b4fab0597773f
Evan Brown <evanbrown@google.com>
2015-09-02 22:24:46 -0700

    unbreak tests

--- flow.groovy
+++ flow.groovy
@@ -1,14 +1,7 @@
 node('docker') {
-  try {
-    def hash = git url: "${GIT_URL}"
-    def app = docker.build("${hash}")
-    app.withRun {c ->
-      sh "docker logs -f ${c.id}"
-    }
-  } catch(e) {
-    def w = new StringWriter()
-    e.printStackTrace(new PrintWriter(w))
-    mail subject: "Build failed with ${e.message}", to: '${OWNER_EMAIL}', body: "Failed: ${w}"
-    throw e
+  def hash = git url: "${GIT_URL}"
+  def app = docker.build("${hash}")
+  app.withRun {c ->
+    sh "docker logs -f ${c.id}"
   }
 }
